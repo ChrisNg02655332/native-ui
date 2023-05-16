5741288ecad58130bf70b4ade4e17f8439abbbc7
ducquang2 <nducquangg@gmail.com>
2023-05-16 14:09:14 +0700

    feat: add TextInput + SecureTextInput

---
+++ src/SecureTextInput.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { useTheme } from './context'
+import { TextInput, TextInputProps } from './TextInput'
+
+export { SecureTextInput }
+
+function SecureTextInput({ ...rest }: TextInputProps) {
+  const { colors } = useTheme()
+  const [hide, setHide] = React.useState(true)
+
+  return (
+    <TextInput
+      secureTextEntry={hide}
+      rightIcon={{
+        type: 'ico',
+        name: !hide ? 'eye-off' : 'eye',
+        color: colors.neutral[400],
+        size: 18,
+        onPress: () => setHide(!hide),
+      }}
+      {...rest}
+    />
+  )
+}
