98dc075a189a23f64f65c98abe21fdced1750149
ducquang2 <nducquangg@gmail.com>
2023-05-19 17:47:09 +0700

    feat: add storybook + stories

---
+++ example/stories/Button.stories.tsx
@@ -0,0 +1,36 @@
+import { View } from 'react-native'
+
+import { Button as ButtonUI } from 'native-uikit'
+import type { ComponentMeta, ComponentStory } from '@storybook/react-native'
+
+export default {
+  title: 'Design System/Elements',
+  component: ButtonUI,
+  decorators: [
+    (Story: any) => (
+      <View style={{ padding: 22, flex: 1 }}>
+        <Story />
+      </View>
+    ),
+  ],
+  args: {
+    size: 'normal',
+    title: 'Button',
+  },
+} as ComponentMeta<typeof ButtonUI>
+
+const types = ['clear', 'solid', 'outline']
+
+const Template: ComponentStory<typeof ButtonUI> = (args) => (
+  <View style={{ justifyContent: 'center', alignItems: 'center', gap: 30 }}>
+    {types.map((type: any) => (
+      <View key={type} style={{ gap: 10 }}>
+        <ButtonUI type={type} title="Button" />
+        <ButtonUI type={type} title="Button" disabled />
+      </View>
+    ))}
+    <ButtonUI {...args} />
+  </View>
+)
+
+export const Button = Template.bind({})
